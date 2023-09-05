4c96991257d20bccfc923e760988633cf0833e8b
du yeong <doyeong32@gmail.com>
2023-09-05 12:01:30 +0900

    권한설정 및 표현식

--- basicsecurity/src/main/java/io/security/basicsecurity/IndexController.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/IndexController.java
@@ -11,7 +11,19 @@ public class IndexController {
         return "index";
     }
 
-    @GetMapping("loginPage")
-    public String loginPage(){return "loginPage";}
+    @GetMapping("/user")
+    public String user(){
+        return "user";
+    }
+
+    @GetMapping("/admin")
+    public String admin(){
+        return "admin";
+    }
+
+    @GetMapping("/admin/pay")
+    public String adminpay(){
+        return "adminpay";
+    }
 
 }
--- basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
+++ basicsecurity/src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -4,43 +4,40 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
-import javax.servlet.http.HttpSession;
+import java.util.Arrays;
 
 @EnableWebSecurity
 public class SecurityConfig {
 
-    @Autowired
-    UserDetailsService userDetailsService;
-
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests()
-//                .antMatchers("/login**").permitAll()
+        http
+                .authorizeRequests()
+                .antMatchers("/user").hasRole("USER")
+                .antMatchers("/admin/pay").access("hasRole('ADMIN')")
+                .antMatchers("/admin/**").access("hasRole('ADMIN') or hasRole('SYS')")
                 .anyRequest().authenticated();
 
         http.formLogin();
 
-        http.logout()
-//                .logoutUrl("/logoutexe")
-                .logoutSuccessUrl("/login")
-                .deleteCookies("JSESSIONID")
-                .addLogoutHandler((request, response, authentication) -> {
-                    HttpSession session = request.getSession();
-                    session.invalidate();
-                    SecurityContext context = SecurityContextHolder.getContext();
-                    SecurityContextHolder.clearContext();
-                    context.setAuthentication(null);
-                })
-                .logoutSuccessHandler((request, response, authentication) -> {
-                    System.out.println("logout is succeed");
-                    response.sendRedirect("/login");
-                });
+        http.userDetailsService(userDetailsService());
 
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService(){
+
+        UserDetails user1 = User.withUsername("user").password("{noop}1111").roles("USER").build();
+        UserDetails user2 = User.withUsername("sys").password("{noop}1111").roles("SYS").build();
+        UserDetails user3 = User.withUsername("admin").password("{noop}1111").roles("ADMIN").build();
+
+        return new InMemoryUserDetailsManager(Arrays.asList(user1,user2,user3));
     }
 }
